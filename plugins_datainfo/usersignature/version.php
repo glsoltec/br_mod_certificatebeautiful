84abaff2ec419e8f6259e16571ca7578cd6b2a2c
root <root@vmi3018606.contaboserver.net>
2026-07-26 01:34:31 -0300

    fix: ajusta dependencia do subplugin usersignature para 2026042700
    O dependency check do Moodle ocorre durante o scan, antes do upgrade.
    Com 2026072501, a dependencia falhava e o subplugin era ignorado.
    Reduzindo para 2026042700 (versao anterior do mod) o subplugin
    eh detectado e instalado junto com o upgrade do modulo principal.

--- plugins_datainfo/usersignature/version.php
+++ plugins_datainfo/usersignature/version.php
@@ -5,7 +5,7 @@ $plugin->component    = 'certificatebeautifuldatainfo_usersignature';
 $plugin->version      = 2026072500;
 $plugin->requires     = 2021041900;
 $plugin->dependencies = [
-    'mod_certificatebeautiful' => 2026072501,
+    'mod_certificatebeautiful' => 2026042700,
 ];
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release  = '2.1.0';
