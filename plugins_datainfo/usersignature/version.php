3f7b63a799b0768e2087f3d89b3bbe124124033c
pascoalfreitas <pstephanine@gmail.com>
2026-07-28 00:28:51 -0300

    Fix bundled usersignature plugin version

--- plugins_datainfo/usersignature/version.php
+++ plugins_datainfo/usersignature/version.php
@@ -2,10 +2,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'certificatebeautifuldatainfo_usersignature';
-$plugin->version      = 2026072500;
-$plugin->requires     = 2021041900;
+$plugin->version      = 2026072801;
+$plugin->requires     = 2025041400;
 $plugin->dependencies = [
-    'mod_certificatebeautiful' => 2026042700,
+    'local_usersignature' => 2026070302,
+    'mod_certificatebeautiful' => ANY_VERSION,
 ];
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release  = '2.1.0';
+$plugin->release  = '2.1.1';
