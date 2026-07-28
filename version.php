30208a4499947b8bd0d418ea5566eeb724be3810
pascoalfreitas <pstephanine@gmail.com>
2026-07-28 00:24:27 -0300

    Unify user signature preferences

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026072800;
-$plugin->release = "3.3.2";
+$plugin->version = 2026072801;
+$plugin->release = "3.3.3";
 $plugin->requires = 2021041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "mod_certificatebeautiful";
